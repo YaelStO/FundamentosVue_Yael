613a6c7ea795df7167bdbd347aeeef607d438444
Yael de Jesús Santiago Ortiz <125827570+YaelStO@users.noreply.github.com>
2025-03-20 22:24:57 -0600

    Subiendo proyecto fundamentosVue_Yael

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup>
+//metodos  
+</script>
+
+<template>
+  <!--Estuctura html-->
+  <h1>Hola Mundo</h1>
+  <button class="btn">HolaBoton</button>
+
+</template>
+
+<style scoped>
+/*estilos css*/
+.h1{
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-size: 45 px;
+  color: red;
+
+}
+
+.btn{
+  border-radius: 15 px;
+  background-color: #ffff;
+  color: black;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+
+}
+</style>
